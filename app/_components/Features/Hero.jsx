b42ae8b5130aa5301340c0f9023aa2a19a2826a6
Big Dahn <harkinborholarholuwah@gmail.com>
2025-11-13 12:00:58 +0100

    features page animated

--- app/_components/Features/Hero.jsx
+++ app/_components/Features/Hero.jsx
@@ -1,10 +1,31 @@
+"use client";
+
 import Image from "next/image";
 import bg from "@/public/assets/features/desktop/hero.jpg";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 function Hero() {
+  useGSAP(() => {
+    gsap.set(".features", {
+      opacity: 0,
+      x: -100,
+    });
+    gsap.set(".img", {
+      opacity: 0,
+      x: 100,
+    });
+    gsap.to(".features, .img", {
+      opacity: 1,
+      x: 0,
+      duration: 1.08,
+      stagger: 0.06,
+    });
+  }, []);
+
   return (
-    <main className="flex h-[490px] w-full m-auto  ">
-      <div className="bg-black h-[490px] w-[45%] relative">
+    <main className="flex h-[490px] w-full m-auto overflow-hidden ">
+      <div className="bg-black h-[490px] w-[45%] relative features opacity-0">
         <div className="w-1.5  bg-[linear-gradient(27deg,#FFC593_0%,#BC7198_43.29%,#5A77FF_83.33%)] top-[37%] h-36 absolute "></div>
         <div className="max-w-[387px] flex flex-col gap-[3em] justify-center items-start m-auto h-full">
           <div className="flex flex-col gap-[0.6em]">
@@ -19,12 +40,10 @@ function Hero() {
           </div>
         </div>
       </div>
-      <div className="relative min-w-[55%]">
+      <div className="relative min-w-[55%] img opacity-0">
         <Image
           src={bg}
           alt="hero"
-          // width={830}
-          // height={650}
           quality={100}
           className="object-cover"
           fill
